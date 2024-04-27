ba5b23623a4d3711e9c63b7cf47aa1c72544f949
Miquel van Smoorenburg <mike@langeraar.net>
2024-04-27 19:02:55 +0200

    update examples

--- examples/naratest.rs
+++ examples/naratest.rs
@@ -1,46 +1,85 @@
-use std::thread::sleep;
+use std::thread::sleep as std_sleep;
 use std::time::Duration;
 
 use nara::io::AsyncReadExt;
 use nara::runtime::Runtime;
-use nara::task::spawn_blocking;
+use nara::task;
+use nara::time::sleep;
 use nara::net::TcpStream;
+use nara::sync::mpsc::channel;
 
-fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
-    let runtime = Runtime::new().unwrap();
-    let n = runtime.block_on(async {
+async fn test_channel() {
+    let (tx, rx) = channel(10);
 
-        let b1 = bl_spawn(1);
-        let b2 = bl_spawn(2);
-        let b3 = bl_spawn(3);
-        let b4 = bl_spawn(4);
-        println!("test1: spawn_blocking x 4 returned: {:?}", futures::join!(b1, b2, b3, b4));
+    let ping = task::spawn(async move {
+        println!("test_channel: start sender");
+        for i in 1 ..=10 {
+            tx.send(i).await.unwrap();
+            sleep(Duration::from_millis(100)).await;
+        }
+    });
 
-        println!("test2: timer 1 sec");
-        nara::time::sleep(Duration::from_millis(1000)).await;
-        println!("test2: timer done");
+    let pong = task::spawn(async move {
+        println!("test_channel: start receiver");
+        while let Some(num) = rx.recv().await {
+            println!("test_channel: recv {num}");
+        }
+    });
 
-        println!("test3: tcp connection");
+    let _ = ping.await;
+    let _ = pong.await;
+    println!("test_channel: done");
+}
+
+async fn test_spawn_blocking() {
+    async fn bl_spawn(id: u64) -> String {
+        task::spawn_blocking(move || {
+            std_sleep(Duration::from_millis(250));
+            format!("foo {}", id)
+        }).await.unwrap()
+    }
+    let b1 = bl_spawn(1);
+    let b2 = bl_spawn(2);
+    let b3 = bl_spawn(3);
+    let b4 = bl_spawn(4);
+    println!("test_spawn_blocking: returned: {:?}", futures::join!(b1, b2, b3, b4));
+}
+
+async fn test_sleep() {
+    println!("test_sleep: timer 1 sec");
+    sleep(Duration::from_millis(1000)).await;
+    println!("test_sleep: timer done");
+}
+
+async fn test_tcp() -> std::io::Result<()> {
+        println!("test_tcp: open tcp connection");
         let mut tcp = TcpStream::connect(("smtp.bit.nl", 25)).await?;
-        println!("test3: connected!");
+        println!("test_tcp: connected!");
         let mut buffer: [u8; 256] = [0; 256];
         while let Ok(len) = tcp.read(&mut buffer).await {
             if len == 0 {
                 break;
             }
-            println!("test3: {:?}", std::str::from_utf8(&buffer[..len]));
+            println!("test_tcp: {:?}", std::str::from_utf8(&buffer[..len]));
             break;
         }
+        Ok(())
+}
+
+fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+    let runtime = Runtime::new().unwrap();
+    let n = runtime.block_on(async {
+
+        test_channel().await;
+        test_sleep().await;
+        test_spawn_blocking().await;
+        test_sleep().await;
+        let _ = test_tcp().await.map_err(|e| println!("test_tcp: error: {}", e));
         return Ok::<_, std::io::Error>(3u32);
-    })?;
-    println!("test4: spawn_blocking return value {n}");
+    });
+
+    println!("final: block_on return value {:?}", n);
 
     Ok(())
 }
 
-async fn bl_spawn(id: u64) -> String {
-    spawn_blocking(move || {
-        sleep(Duration::from_millis(250));
-        format!("foo {}", id)
-    }).await.unwrap()
-}
