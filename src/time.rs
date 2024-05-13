681388044fc44ecef5c3a1938f1e3200e3f83b17
Miquel van Smoorenburg <mike@langeraar.net>
2024-05-13 21:49:37 +0200

    time.rs: make Rc<Refcell>> etc the same as everywhere else

--- src/time.rs
+++ src/time.rs
@@ -1,4 +1,4 @@
-use std::cell::{Cell, RefCell};
+use std::cell::RefCell;
 use std::collections::BTreeMap;
 use std::future::Future;
 use std::task::{Context, Poll};
@@ -8,26 +8,26 @@ use std::task::Waker;
 use std::time::{Duration, Instant};
 
 pub(crate) struct Timer {
-    inner:  Rc<InnerTimer>,
+    inner:  Rc<RefCell<InnerTimer>>,
 }
 
 pub(crate) struct InnerTimer {
-    timers: RefCell<BTreeMap::<Sleep, Option<Waker>>>,
-    next_id: Cell<u64>,
+    timers: BTreeMap::<Sleep, Option<Waker>>,
+    next_id: u64,
 }
 
 thread_local! {
     // Valid after Timer::activate(), invalid after Timer::deactivate()
-    static TIMER: RefCell<Weak<InnerTimer>> = RefCell::new(Weak::new());
+    static TIMER: RefCell<Weak<RefCell<InnerTimer>>> = RefCell::new(Weak::new());
 }
 
 impl Timer {
     // Return a new Timer.
     pub fn new() -> Timer {
-        let inner = Rc::new(InnerTimer {
-            timers: RefCell::new(BTreeMap::new()),
-            next_id: Cell::new(1),
-        });
+        let inner = Rc::new(RefCell::new(InnerTimer {
+            timers: BTreeMap::new(),
+            next_id: 1,
+        }));
         Timer { inner }
     }
 
@@ -44,17 +44,17 @@ impl Timer {
     // Return how long it will take until the next timer goes off.
     // This is used by the reactor as a timeout.
     pub fn next_deadline(&self) -> Option<Duration> {
-        let timers = self.inner.timers.borrow();
-        let (first, _) = timers.first_key_value()?;
+        let this = self.inner.borrow();
+        let (first, _) = this.timers.first_key_value()?;
         let now = Instant::now();
         Some(first.deadline.checked_duration_since(now).unwrap_or(Duration::ZERO))
     }
 
     // Wake waiters on epired timers.
     pub fn tick(&self) {
-        let mut timers = self.inner.timers.borrow_mut();
+        let mut this = self.inner.borrow_mut();
         let now = Instant::now();
-        while let Some(entry) = timers.first_entry() {
+        while let Some(entry) = this.timers.first_entry() {
             if entry.key().deadline > now {
                 break;
             }
@@ -87,10 +87,11 @@ impl Sleep {
 pub fn sleep_until(deadline: Instant) -> Sleep {
     TIMER.with_borrow(|t| {
         let timer = t.upgrade().unwrap();
-        let id = timer.next_id.get();
-        timer.next_id.set(id + 1);
+        let mut this = timer.borrow_mut();
+        let id = this.next_id;
+        this.next_id += 1;
         let key = Sleep { deadline, id };
-        timer.timers.borrow_mut().insert(key.clone(), None);
+        this.timers.insert(key.clone(), None);
         key
     })
 }
@@ -104,10 +105,10 @@ impl Future for Sleep {
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let timer = TIMER.with_borrow(|t| t.upgrade().unwrap());
-        let mut timers = timer.timers.borrow_mut();
+        let mut this = timer.borrow_mut();
         // Note, if there is an entry in `timers`, it means that this was
         // a spurious wakeup, not caused by Timer::tick().
-        match timers.get_mut(self.get_mut()) {
+        match this.timers.get_mut(self.get_mut()) {
             None => Poll::Ready(()),
             Some(e) => {
                 // Only update the entry if it was not set yet.
